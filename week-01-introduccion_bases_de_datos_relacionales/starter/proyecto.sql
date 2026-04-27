70a8c2b11e452d2a053a1418374ed2518d3a6930
crissand16 <cristian.a.gomez16@gmail.com>
2026-04-27 11:37:28 -0500

    proyect_1

--- week-01-introduccion_bases_de_datos_relacionales/starter/proyecto.sql
+++ week-01-introduccion_bases_de_datos_relacionales/starter/proyecto.sql
@@ -1,5 +1,5 @@
 --Crea Tabla Residentes.
-CREATE TABLE residents (
+CREATE TABLE IF NOT EXISTS residents (
     id_resident         INTEGER PRIMARY KEY,
     name_resident       TEXT NOT NULL,
     age_resident        INTEGER,
@@ -8,7 +8,7 @@ CREATE TABLE residents (
 );
 
 --Crea Tabla Cuidadores
-CREATE TABLE caregivers (
+CREATE TABLE IF NOT EXISTS caregivers (
     id_caregiver         INTEGER PRIMARY KEY,
     name_caregiver       TEXT    NOT NULL,
     turno_caregiver      TEXT,
@@ -17,29 +17,29 @@ CREATE TABLE caregivers (
 );
 
 --Insertamos datos en la tabla residentes
-INSERT INTO residents (id_resident, name_resident, age_resident, room_resident, entry_date_resident) VALUES
-(1, 'María López', 78, '101A', '2024-01-10'),
-(2, 'Carlos Pérez', 82, '102B', '2023-11-05'),
-(3, 'Ana Gómez', 75, '103A', '2024-02-20'),
-(4, 'Luis Torres', 80, '104C', '2023-12-15'),
-(5, 'Rosa Martínez', 77, '105B', '2024-03-01');
+INSERT INTO residents (name_resident, age_resident, room_resident, entry_date_resident) VALUES
+('María López', 78, '101A', '2024-01-10'),
+('Carlos Pérez', 82, '102B', '2023-11-05'),
+('Ana Gómez', 75, '103A', '2024-02-20'),
+('Luis Torres', 80, '104C', '2023-12-15'),
+('Rosa Martínez', 77, '105B', '2024-03-01');
 
 --Insertamos datos en la tabla Cuidadores
-INSERT INTO carevigers (id_caregiver, name_caregiver, turno_caregiver, email_caregiver, phone_caregiver) VALUES
-(1, 'Juan Rodríguez', 'Día', 'juan.rodriguez@email.com', '3001234567'),
-(2, 'Laura Sánchez', 'Noche', 'laura.sanchez@email.com', '3007654321'),
-(3, 'Pedro Ramírez', 'Día', 'pedro.ramirez@email.com', '3011111111'),
-(4, 'Sofía Herrera', 'Tarde', 'sofia.herrera@email.com', '3022222222'),
-(5, 'Miguel Castro', 'Noche', 'miguel.castro@email.com', '3033333333');
+INSERT INTO caregivers (name_caregiver, turno_caregiver, email_caregiver, phone_caregiver) VALUES
+('Juan Rodríguez', 'Día', 'juan.rodriguez@email.com', '3001234567'),
+('Laura Sánchez', 'Noche', 'laura.sanchez@email.com', '3007654321'),
+('Pedro Ramírez', 'Día', 'pedro.ramirez@email.com', '3011111111'),
+('Sofía Herrera', 'Tarde', 'sofia.herrera@email.com', '3022222222'),
+('Miguel Castro', 'Noche', 'miguel.castro@email.com', '3033333333');
 
 --Mostrar todos los residentes con todas sus columnas
 SELECT *
 FROM   residents;
 
 --Mostrar solo el nombre de los residentes ordenados alfabéticamente
-SELECT ...
+SELECT name_resident, age_resident
 FROM   residents
-ORDER BY ...;
+ORDER BY name_resident DESC;
 
 --Contar cuántos items tienes en total
 SELECT COUNT(*) AS total_residents
